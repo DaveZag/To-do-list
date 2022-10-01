966b82be968c0d041bad6f1b28c1d4cc09844625
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2022-10-01 19:00:46 +0200

    Create deployment branch

--- app/modules/display_tasks_onAdd.js
+++
@@ -1,7 +0,0 @@
-import displayTask from './diplay.js';
-
-function displayOnAdd(task) {
-  displayTask(task);
-}
-
-export default displayOnAdd;
--- app/modules/task_template.js
+++
@@ -1,9 +0,0 @@
-class Task {
-  constructor(description) {
-    this.desc = description;
-    this.id = 0;
-    this.completed = false;
-  }
-}
-
-export default Task;
