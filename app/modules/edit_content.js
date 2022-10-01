ca58a9fd84d6470b1a88f2301d1299b6798af565
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2022-10-01 15:38:52 +0200

    Fix bugs

--- app/modules/edit_content.js
+++ app/modules/edit_content.js
@@ -6,8 +6,8 @@ export default function editTask() {
   let tasks;
 
   if (
-    localStorage.getItem('tasks') !== null
-    && localStorage.getItem('tasks') !== '[]'
+    localStorage.getItem('tasks') !== null ||
+    localStorage.getItem('tasks') !== '[]'
   ) {
     tasks = JSON.parse(localStorage.getItem('tasks'));
   }
@@ -29,3 +29,34 @@ export default function editTask() {
     });
   });
 }
+
+// import { editable } from './selectors.js';
+
+// export default function editTask() {
+//   const arrEdit = [...editable];
+
+//   let tasks;
+//   if (
+//     localStorage.getItem('tasks') !== null &&
+//     localStorage.getItem('tasks') !== '[]'
+//   ) {
+//     tasks = JSON.parse(localStorage.getItem('tasks'));
+//   }
+
+//   arrEdit.forEach((element) => {
+//     const itemToEdit = element.innerText;
+//     let editedText;
+//     let editedArr;
+//     element.addEventListener('input', () => {
+//       editedText = element.innerText.trim();
+//       editedArr = tasks.map((task) => {
+//         if (task.desc === itemToEdit) {
+//           //  return modified object property
+//           return { ...task, desc: `${editedText}` };
+//         }
+//         return task;
+//       });
+//       localStorage.setItem('tasks', JSON.stringify(editedArr));
+//     });
+//   });
+// }
