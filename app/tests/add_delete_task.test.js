ae88b0f05ec5a66fa521aabc0f3761cb3ff8aeeb
Yasir Khan <khanyaser007@gmail.com>
2022-10-13 18:45:57 +0500

    Add test for delete function in add_delete_task.test.js file

--- app/tests/add_delete_task.test.js
+++ app/tests/add_delete_task.test.js
@@ -1,5 +1,5 @@
 import add from '../modules/add.js';
-// import deleteFromDOM from '../modules/delete_from_DOM.js';
+import deleteFromDOM from '../modules/delete_from_DOM.js';
 
 describe('add and delete test', () => {
   test(' Adds properly a task', () => {
@@ -19,4 +19,14 @@ describe('add and delete test', () => {
 
     expect(document.getElementsByClassName('todo')).toHaveLength(1);
   });
+
+  test('Deletes properly from Dom and localStorage', () => {
+    document.getElementsByClassName('todo');
+    const todo = document.querySelector('.todo');
+    deleteFromDOM(todo);
+
+    expect(document.getElementsByClassName('todo')).toHaveLength(0);
+    expect(localStorage.getItem('tasks')).toBe('[]');
+  });
+
 });
